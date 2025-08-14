6eb70ae0cb4562f209b7f4a30531b55dee9c62fe
Tharika Dahanayake <tharikad2002@gmail.com>
2025-08-14 13:03:50 +0530

    TCP Communication

---
+++ TcpClientApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TcpClientApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Console.WriteLine("Connecting to server...");
+
+            TcpClient client = new TcpClient();
+            await client.ConnectAsync("127.0.0.1", 5000);
+
+            NetworkStream stream = client.GetStream();
+
+            // Send message
+            string message = "Hello from client!";
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            await stream.WriteAsync(data, 0, data.Length);
+
+            // Read response
+            byte[] buffer = new byte[1024];
+            int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            Console.WriteLine("Server replied: " + response);
+
+            Console.WriteLine("Press ENTER to close connection.");
+            Console.ReadLine();
+
+            client.Close();
+        }
+    }
+}
+
+
+
---
+++ TcpServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+
+namespace TcpServer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+
+        {
+            var ip = IPAddress.Any;
+            var listener = new TcpListener(ip, 5000);
+            listener.Start();
+            Console.WriteLine("Server started. Waiting for connection.." + ip);
+
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                Console.WriteLine("Client connected!");
+
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                string received = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Recieved from client:{received}");
+
+                string response = $"Hello, client! You said: {received}";
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
+
+
+                client.Close();
+
+            }
+            
+        }
+    }
+}
